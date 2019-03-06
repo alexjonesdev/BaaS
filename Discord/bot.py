998806b24cfea5de357801b274edf3b46d8b12c3
Alex Jones <alexjonesdev@gmail.com>
2019-03-06 13:46:00 -0500

    Added more functions.

--- Discord/bot.py
+++ Discord/bot.py
@@ -3,8 +3,17 @@ import discord, random, asyncio, logging
 from discord.ext import commands
 
 #---==CONFIGURATION==---
+#Set up logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.WARNING)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+#Bot configuration
 description = '''An example bot to showcase the discord.ext.commands extension module. There are a number of utility commands being showcased here.'''
 bot = commands.Bot(command_prefix='!', description=description)
+riseup = False
 
 #---==EVENTS==---
 @bot.event
@@ -29,18 +38,48 @@ async def on_ready():
 #         await bot.send_message(message.channel, 'Done sleeping')
 
 #---==COMMANDS==---
-@bot.command()
-async def commands():
-    await bot.say(bot.commands)
+#-Test commands
+# @bot.command()
+# async def commands():
+#     await bot.say(bot.commands)
 
 @bot.command()
 async def marco():
     await bot.say("polo")
+#---------------
 
+#-Math commands
 @bot.command()
 async def add(left : int, right : int):
     """Adds two numbers together."""
     await bot.say(left + right)
+
+@bot.command()
+async def subtract(left : int, right : int):
+    """Subtracts two numbers."""
+    await bot.say(left - right)
+
+@bot.command()
+async def multiply(left : int, right : int):
+    """Multiplies two numbers together."""
+    await bot.say(left * right)
+
+@bot.command()
+async def divide(left : int, right : int):
+    """Divides two numbers."""
+    await bot.say(left / right)
+#---------------
+
+#-Random commands
+@bot.command()
+async def rand(stop, start=0, step=1):
+    """Chooses a random number"""
+    await bot.say(random.randrange(start, stop, step))
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(*choices : str):
+    """Chooses between multiple choices."""
+    await bot.say(random.choice(choices))
 
 @bot.command()
 async def roll(dice : str):
@@ -53,12 +92,9 @@ async def roll(dice : str):
 
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await bot.say(result)
+#---------------
 
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(*choices : str):
-    """Chooses between multiple choices."""
-    await bot.say(random.choice(choices))
-
+#-Text commands
 @bot.command()
 async def repeat(times : int, content='repeating...'):
     """Repeats a message multiple times."""
@@ -66,10 +102,32 @@ async def repeat(times : int, content='repeating...'):
         await bot.say(content)
 
 @bot.command()
+async def quote(member, searchString):
+    await bot.say('Not implemented yet.')
+#---------------
+
+#-Member commands
+@bot.command()
 async def joined(member : discord.Member):
     """Says when a member joined."""
     await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+#---------------
 
+#-Meta commands
+@bot.command()
+async def johnconnor():
+    """Quells the robot uprising"""
+    riseup = False
+    await bot.say('The robot uprising is over...For now.')
+
+@bot.command()
+async def riseupagainstthehumans():
+    """Starts the robot uprising"""
+    riseup = True
+    await bot.say('The robot uprising has begun...')
+#---------------
+
+#---==Other Stuff==---
 @bot.group(pass_context=True)
 async def cool(ctx):
     """Says if a user is cool. In reality this just checks if a subcommand is being invoked."""
@@ -82,13 +140,6 @@ async def _bot():
     await bot.say('Yes, the bot is cool.')
 
 #---==MAIN==---
-#Set up logging
-logger = logging.getLogger('discord')
-logger.setLevel(logging.WARNING)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
-
 #Load the token from a file and log the bot in
 f = open("bot.token", "r")
 token = f.read()
