b314a017d7ffa6f5534388fbc3c8a4b152614c70
Alex Jones <alexjonesdev@gmail.com>
2020-01-08 10:40:38 -0500

    Set up and configured basic logging.

--- Discord/bot.py
+++ Discord/bot.py
@@ -1,26 +1,35 @@
 #---==IMPORTS==---
-import logging #discord, asyncio,
+import logging, logging.config #discord, asyncio,
 from discord.ext import commands
 from modules import test, math, weather, text, random, member, image
 
 #---==CONFIGURATION==---
-#Set up logging
-logger = logging.getLogger('discord')
-logger.setLevel(logging.WARNING)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
-
-#Bot configuration
 description = '''A bot service made from the discord.py library. Commands:'''
-bot = commands.Bot(command_prefix='!', description=description)
+command_prefix = '!'
+logging_config = 'logging.conf'
+primary_logger = 'discord'
+
+#---==INITIALIZATION==---
+#Set up logging
+logging.config.fileConfig(logging_config)
+logger = logging.getLogger(primary_logger)
+
+#Set up the bot
+bot = commands.Bot(command_prefix=command_prefix, description=description)
 
 #Load modules
-bot.add_cog(test.Test(bot))
-bot.add_cog(math.Math(bot))
-bot.add_cog(random.Random(bot))
-bot.add_cog(text.Text(bot))
-bot.add_cog(member.Member(bot))
+# logger.info('Loading modules...')
+# bot.add_cog(test.Test(bot))
+# logger.info('MODULE LOADED: Test')
+# bot.add_cog(math.Math(bot))
+# logger.info('MODULE LOADED: Math')
+# bot.add_cog(random.Random(bot))
+# logger.info('MODULE LOADED: Random')
+# bot.add_cog(text.Text(bot))
+# logger.info('MODULE LOADED: Text')
+# bot.add_cog(member.Member(bot))
+# logger.info('MODULE LOADED: Member')
+# logger.info('Modules loaded.')
 
 #---==EVENTS==---
 @bot.event
@@ -29,11 +38,18 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    logger.info('Bot has logged in as %s:%s', bot.user.name, bot.user.id)
 
 #---==COMMANDS==---
+@bot.command()
+async def ping(ctx):
+    logger.info('Received !ping command')
+    await ctx.send('pong')
+    logger.info('Responded with "pong"')
 
 #---==MAIN==---
 #Load the token from a file and log the bot in
 f = open("bot.token", "r")
 token = f.read()
+f.close()
 bot.run(token)
